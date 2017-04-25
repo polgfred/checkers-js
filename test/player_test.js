2b74967540b9aebc8054d0ca37a31dfd9938fe2d
Frederick Polgardy <fred@polgardy.com>
2017-04-24 20:06:50 -0600

    another test

---
+++ test/player_test.js
@@ -0,0 +1,44 @@
+import expect from 'expect.js';
+
+import Player from '../src/player';
+
+describe('Player', function() {
+  before(function() {
+    this.player = new Player([
+      [  0,  0,  1,  0,  0,  0,  0,  0 ],
+      [  0, -1,  0, -1,  0,  0,  0,  0 ],
+      [  0,  0,  0,  0,  0,  0,  0,  0 ],
+      [  0,  0,  0, -1,  0, -1,  0,  0 ],
+      [  0,  0,  0,  0,  0,  0,  0,  0 ],
+      [  0,  0,  0,  0,  0, -1,  0,  0 ],
+      [  0,  0,  0,  0,  0,  0,  0,  0 ],
+      [  0,  0,  0,  0,  0,  0,  0,  0 ]
+    ], 1)
+  });
+
+  it('should initialize the player', function() {
+    expect(this.player.rules.board[0][0]).to.be(0);
+  });
+
+  it('should initialize the side', function() {
+    expect(this.player.rules.side).to.be(1);
+  });
+
+  it('should initialize the level', function() {
+    expect(this.player.level).to.be(6);
+  });
+
+  describe('#run', function() {
+    before(function() {
+      this.play = this.player.run();
+    });
+
+    it('should find the best play from this position', function() {
+      expect(this.play[0]).to.eql([ 2, 0, 4, 2, 6, 4, 4, 6 ]);
+    });
+
+    it('should find the best score from this position', function() {
+      expect(this.play[1]).to.be(-68);
+    });
+  });
+});
