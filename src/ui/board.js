f2f9a0051b1844c7de31d3fec26f2c0774c9b9e9
Frederick Polgardy <fred@polgardy.com>
2017-04-24 22:51:37 -0600

    dnd mechanics are actually working!

--- src/ui/board.js
+++ src/ui/board.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { DragDropContext, DragSource, DropTarget } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 
 const pieceImages = new Map();
 pieceImages[+1] = 'src/images/pr.png';
@@ -6,7 +8,8 @@ pieceImages[+2] = 'src/images/kr.png';
 pieceImages[-1] = 'src/images/pw.png';
 pieceImages[-2] = 'src/images/kw.png';
 
-export default class Board extends React.Component {
+@DragDropContext(HTML5Backend)
+export default class Board extends Component {
   static defaultProps = {
     // initial checkerboard setup
     board: [
@@ -39,26 +42,69 @@ export default class Board extends React.Component {
 
     for (let y = 7; y >= 0; --y) {
       elems.push(<tr key={y}>
-        { this.renderColumns(y) }
+        { this.renderCells(y) }
       </tr>);
     }
 
     return elems;
   }
 
-  renderColumns(y) {
+  renderCells(y) {
     let { board } = this.props, elems = [];
 
     for (let x = 0; x <= 7; ++x) {
       let p = board[y][x];
 
       if ((x + y) % 2 == 0) {
-        elems.push(<td key={x} className="playable"><img src={pieceImages[p]} /></td>);
+        elems.push(<Cell key={x} x={x} y={y} p={p} />);
       } else {
         elems.push(<td key={x} />);
       }
     }
 
     return elems;
+  }
+}
+
+let dropTarget = {
+  drop(props, monitor) {
+  }
+};
+
+function dropCollect(connect, monitor) {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver()
+  };
+}
+
+@DropTarget('piece', dropTarget, dropCollect)
+class Cell extends Component {
+  render() {
+    let { x, y, p, connectDropTarget, isOver } = this.props;
+    return connectDropTarget(<td className={`playable ${isOver ? 'dragging' : ''}`}>
+      { p != 0 && <Piece p={p} /> }
+    </td>);
+  }
+}
+
+let dragSource = {
+  beginDrag(props) {
+    return {};
+  }
+};
+
+function dragCollect(connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging()
+  };
+}
+
+@DragSource('piece', dragSource, dragCollect)
+class Piece extends Component {
+  render() {
+    let { p, connectDragSource, isDragging } = this.props;
+    return connectDragSource(<img src={pieceImages[p]} />);
   }
 }
