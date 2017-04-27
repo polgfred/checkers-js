412cac7e599fb1760e651753e55945894456e7d2
Frederick Polgardy <fred@polgardy.com>
2017-04-27 17:20:31 -0600

    initialize the starting position for faster copying

--- src/utils.js
+++ src/utils.js
@@ -1,23 +1,25 @@
 'use strict';
 
+// set up the initial board position
+const initial = as2DArray(new ArrayBuffer(64));
+initial[0][0] = initial[0][2] = initial[0][4] = initial[0][6] = +1;
+initial[1][1] = initial[1][3] = initial[1][5] = initial[1][7] = +1;
+initial[2][0] = initial[2][2] = initial[2][4] = initial[2][6] = +1;
+initial[5][1] = initial[5][3] = initial[5][5] = initial[5][7] = -1;
+initial[6][0] = initial[6][2] = initial[6][4] = initial[6][6] = -1;
+initial[7][1] = initial[7][3] = initial[7][5] = initial[7][7] = -1;
+
+// make a copy of the initial board position
 export function newBoard() {
-  let board = as2DArray(new ArrayBuffer(64));
-
-  // set up the initial board position
-  board[0][0] = board[0][2] = board[0][4] = board[0][6] = +1;
-  board[1][1] = board[1][3] = board[1][5] = board[1][7] = +1;
-  board[2][0] = board[2][2] = board[2][4] = board[2][6] = +1;
-  board[5][1] = board[5][3] = board[5][5] = board[5][7] = -1;
-  board[6][0] = board[6][2] = board[6][4] = board[6][6] = -1;
-  board[7][1] = board[7][3] = board[7][5] = board[7][7] = -1;
-
-  return board;
+  return copyBoard(initial);
 }
 
+// only use this on boards that are backed by a shared buffer!
 export function copyBoard(board) {
   return as2DArray(board[0].buffer.slice());
 }
 
+// make a 2d array wrapper around a 64-byte buffer
 export function as2DArray(buf) {
   return [
     new Int8Array(buf,  0, 8),
