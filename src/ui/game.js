25749ff62eb3ce68afd5994a686a7684d82b414a
Frederick Polgardy <fred@polgardy.com>
2017-04-26 22:28:06 -0600

    make the drop handler deal with multiple jumps

--- src/ui/game.js
+++ src/ui/game.js
@@ -61,20 +61,23 @@ export default class Game extends Component {
       return false;
     }
 
-    return !!plays.find(play => play[0] == x && play[1] == y);
+    return plays.some(([tx, ty]) => x == tx && y == ty);
   }
 
   @autobind
   canDrop(props, item) {
-    let { plays } = this.state,
+    let { player, plays } = this.state,
         { x: nx, y: ny } = props,
         { x, y } = item;
 
-    return !!plays.find(play =>
-              play[0] == x &&
-              play[1] == y &&
-              play[2] == nx &&
-              play[3] == ny);
+    // if computer's turn, don't allow dragging at all
+    if (player == 'computer') {
+      return false;
+    }
+
+    return plays.some(
+              ([tx, ty, tnx, tny]) =>
+                x == tx && y == ty && nx == tnx && ny == tny);
   }
 
   @autobind
@@ -83,28 +86,73 @@ export default class Game extends Component {
         { x: nx, y: ny } = props,
         { x, y } = item;
 
-    if (this.canDrop(props, item)) {
-      this.setState(state => {
-        let { board, side } = state, p = board[y][x];
+    let next = this.checkPlay([x, y, nx, ny]);
 
-        // make the move
-        board[y][x] = 0;
-        board[ny][nx] = p;
-        if (Math.abs(nx - x) == 2) {
-          let mx = (x + nx) / 2, my = (y + ny) / 2;
-          board[my][mx] = 0;
-        }
+    if (!next) {
+      return;
+    }
 
-        // build the next state
-        let nstate = {
-          board: board,
-          side: -side,
-          player: 'human'
-        }
+    this.setState(state => {
+      let { board, side } = state,
+          p = board[y][x],
+          promoted = (p == 1 && ny == 7) || (p == -1 && ny == 0);
 
+      // make the move
+      board[y][x] = 0;
+      board[ny][nx] = promoted ? 2 * p : p;
+
+      // it's a jump, so remove the jumped piece
+      if (Math.abs(nx - x) == 2) {
+        let mx = (x + nx) / 2, my = (y + ny) / 2;
+        board[my][mx] = 0;
+      }
+
+      // build the next state
+      let nstate = {
+        board: board,
+        player: 'human'
+      }
+
+      if (next === true) {
+        // move is done, switch sides
+        nstate.side = -side;
         this.setupRules(nstate);
-        return nstate;
-      });
+      } else {
+        // get plays from this position
+        nstate.plays = next;
+      }
+
+      return nstate;
+    });
+  }
+
+  checkPlay(play) {
+    let { plays, current } = this.state, next = [];
+
+    if (current.length) {
+      play = current.concat(play.slice(2))
+    }
+
+    top:
+    for (let i = 0; i < plays.length; ++i) {
+      let p = plays[i];
+      if (play.length <= p.length) {
+        let j;
+        for (j = 0; j < play.length; ++j) {
+          if (play[j] != p[j]) {
+            continue top;
+          }
+        }
+        if (j == p.length) {
+          return true;
+        } else {
+          next.push(p.slice(j - 2));
+        }
+      }
+    }
+
+    if (next.length) {
+      return next;
     }
   }
 }
