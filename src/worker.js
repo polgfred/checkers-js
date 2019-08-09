6d09826d20cf33a8d6ec2afe829a762f14542e07
Frederick Polgardy <fred@polgardy.com>
2019-08-09 09:28:20 -0600

    remove code and just publish the generated stuff

--- src/worker.js
+++
@@ -1,6 +0,0 @@
-import Analyzer from './core/analyzer';
-
-addEventListener('message', ev => {
-  let { board, side } = ev.data, analyzer = new Analyzer(board, side);
-  postMessage({ move: analyzer.run()[0] });
-}, false);
