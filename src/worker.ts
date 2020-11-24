bff791d215e3efe3e82606d899481981ea6b4670
Frederick Polgardy <fred@polgardy.com>
2020-11-23 18:28:11 -0700

    fix worker scope in postMessage

--- src/worker.ts
+++ src/worker.ts
@@ -1,13 +1,16 @@
 import { Board } from './core/rules';
 import { analyze } from './core/analyzer';
 
-addEventListener(
+// tell typescript that we're in a web worker, as the
+// postMessage API is slightly different
+declare const self: DedicatedWorkerGlobalScope;
+
+self.addEventListener(
   'message',
   (ev: { data: { board: Board; side: number } }) => {
     const { board, side } = ev.data;
     const [move] = analyze(board, side);
-    // @ts-ignore
-    postMessage({ move });
+    self.postMessage({ move });
   },
   false
 );
