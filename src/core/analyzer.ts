1e2b42b64bc259d442f01828ce6b3f3da0e11bcd
Frederick Polgardy <fred@polgardy.com>
2020-11-24 22:38:35 -0700

    clean up analyzer and add a test

--- src/core/analyzer.ts
+++ src/core/analyzer.ts
@@ -8,30 +8,34 @@ const { RED, WHT } = SideType;
 // how many levels deep to search the tree
 const LEVEL = 8;
 
-export function analyze(board: BoardType, side: SideType): [MoveType, number] {
+export function analyze(
+  board: BoardType,
+  side: SideType,
+  player: Evaluator = defaultEvaluator
+): [MoveType, number] {
   // make the rules for the current position
   const { getBoard, getSide, findJumps, doJump, findMoves, doMove } = makeRules(
     board,
     side
   );
 
-  function loop(level: number, player: Evaluator): [MoveType, number] {
+  function loop(level: number): [MoveType, number] {
     const board = getBoard();
     const side = getSide();
     let bestScore = side / -0;
     let bestPlay: MoveType;
     let current: number;
 
-    // always try to find counter-jumps from this position
+    // if there are jumps from this position, keep searching
     const jumps = findJumps();
-
     if (jumps.length) {
+      // analyze counter-jumps from this position
       for (let i = 0; i < jumps.length; ++i) {
         const jump = jumps[i];
 
         // perform the jump and descend a level
         const reverse = doJump(jump);
-        current = loop(level - 1, player)[1];
+        current = loop(level - 1)[1];
         reverse();
 
         // keep track of the best move from this position
@@ -43,40 +47,37 @@ export function analyze(board: BoardType, side: SideType): [MoveType, number] {
           bestScore = current;
         }
       }
-    } else {
-      // see if we've hit bottom
-      if (level <= 0) {
-        // return score for this position
-        bestScore = player.evaluate(board);
-      } else {
-        // find counter-moves from this position
-        const moves = findMoves();
+    } else if (level > 0) {
+      // analyze counter-moves from this position
+      const moves = findMoves();
 
-        for (let i = 0; i < moves.length; ++i) {
-          const move = moves[i];
+      for (let i = 0; i < moves.length; ++i) {
+        const move = moves[i];
 
-          // perform the jump and descend a level
-          const reverse = doMove(move);
-          current = loop(level - 1, player)[1];
-          reverse();
+        // perform the jump and descend a level
+        const reverse = doMove(move);
+        current = loop(level - 1)[1];
+        reverse();
 
-          // keep track of the best move from this position
-          if (
-            (side === RED && current > bestScore) ||
-            (side === WHT && current < bestScore)
-          ) {
-            bestPlay = move;
-            bestScore = current;
-          }
+        // keep track of the best move from this position
+        if (
+          (side === RED && current > bestScore) ||
+          (side === WHT && current < bestScore)
+        ) {
+          bestPlay = move;
+          bestScore = current;
         }
       }
+    } else {
+      // we've hit bottom and there are no jumps, so just return
+      // the score for this position
+      bestScore = player.evaluate(board);
     }
 
     // a pair representing the winning play and score for this turn
     return [bestPlay, bestScore];
   }
 
-  // start at the top level
-  // use default evaluator for both sides (but feel free to change it)
-  return loop(LEVEL, defaultEvaluator);
+  // start the descent
+  return loop(LEVEL);
 }
--- src/core/performance.test.ts
+++ src/core/performance.test.ts
@@ -1,7 +1,12 @@
 /* eslint-disable no-console */
 import process from 'process';
+
+import { SideType } from './types';
 import { makeRules } from './rules';
+import { analyze } from './analyzer';
 import { newBoard, newBoardFromData } from './utils';
+
+const { RED, WHT } = SideType;
 
 function duration(
   action: () => void,
@@ -17,16 +22,16 @@ function duration(
 describe('Performance', () => {
   describe('moves', () => {
     it('should find the moves from this position', () => {
-      const { findMoves } = makeRules(newBoard(), 1);
+      const { findMoves } = makeRules(newBoard(), RED);
       const [sec, nsec] = duration(findMoves, 1000);
-      console.log(`findMoves: ${sec}s ${nsec}ns`);
+      console.log('findMoves', sec + nsec / 1e9);
       expect(sec * 1e9 + nsec).toBeGreaterThan(0);
     });
 
     it('should find the jumps from this position', () => {
-      const { findJumps } = makeRules(newBoard(), 1);
+      const { findJumps } = makeRules(newBoard(), RED);
       const [sec, nsec] = duration(findJumps, 1000);
-      console.log(`findJumps: ${sec}s ${nsec}ns`);
+      console.log('findJumps', sec + nsec / 1e9);
       expect(sec * 1e9 + nsec).toBeGreaterThan(0);
     });
   });
@@ -35,23 +40,52 @@ describe('Performance', () => {
     it('should find the jumps from this position', () => {
       const { findJumps } = makeRules(
         newBoardFromData(
+          // prettier-ignore
           [
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, -1, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, -1, 0, -1, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, -1, 0, -1, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0, 0],
+            [ 0,  0,  0,  0,  0,  0,  0,  0 ],
+            [ 0,  0,  0,  0,  0,  0,  0,  0 ],
+            [ 0,  0,  0,  0,  0, -1,  0,  0 ],
+            [ 0,  0,  0,  0,  0,  0,  0,  0 ],
+            [ 0,  0,  0, -1,  0, -1,  0,  0 ],
+            [ 0,  0,  0,  0,  0,  0,  0,  0 ],
+            [ 0, -1,  0, -1,  0,  0,  0,  0 ],
+            [ 0,  0,  1,  0,  0,  0,  0,  0 ],
           ].reverse()
         ),
-        1
+        RED
       );
 
       const [sec, nsec] = duration(findJumps, 1000);
-      console.log(`findJumps: ${sec}s ${nsec}ns`);
+      console.log('findJumps', sec + nsec / 1e9);
       expect(sec * 1e9 + nsec).toBeGreaterThan(0);
+    });
+  });
+
+  describe('analyzer', () => {
+    it('should return a move and score from this position', () => {
+      const start = process.hrtime();
+      const [move] = analyze(
+        newBoardFromData(
+          // prettier-ignore
+          [
+            [ 0,  0,  0, -1,  0, -1,  0,  0 ],
+            [-1,  0,  0,  0,  0,  0,  0,  0 ],
+            [ 0,  0,  0, -1,  0,  0,  0,  0 ],
+            [ 1,  0,  1,  0,  0,  0, -1,  0 ],
+            [ 0,  1,  0,  0,  0, -1,  0,  0 ],
+            [ 1,  0,  0,  0,  0,  0,  0,  0 ],
+            [ 0,  0,  0,  0,  0,  0,  0,  1 ],
+            [ 0,  0,  0,  0, -2,  0,  1,  0 ],
+          ].reverse()
+        ),
+        WHT
+      );
+      const [sec, nsec] = process.hrtime(start);
+      console.log('analyze', sec + nsec / 1e9);
+      expect(move).toEqual([
+        [3, 7],
+        [2, 6],
+      ]);
     });
   });
 });
