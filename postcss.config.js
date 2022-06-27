10af80a9b5485aebf23152afdff4df8e4ea499f5
Frederick Polgardy <fred@polgardy.com>
2022-06-27 21:01:13 +0000

    updates to react 18 (roots) and postcss fixes

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,11 @@
 module.exports = {
-  plugins: {
-    'postcss-preset-env': {
-      stage: 4,
-      autoprefixer: true,
-      preserve: false,
-      features: {
-        'nesting-rules': true,
-        'color-mod-function': true,
+  plugins: [
+    [
+      'postcss-preset-env',
+      {
+        stage: 0,
+        preserve: false,
       },
-    },
-  },
+    ],
+  ],
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import { Game } from './ui/game';
 
@@ -11,7 +11,9 @@ import './main.css';
 document.addEventListener(
   'DOMContentLoaded',
   () => {
-    ReactDOM.render(<Game />, document.getElementById('checkers-container'));
+    const container = document.getElementById('checkers-container');
+    const root = createRoot(container);
+    root.render(<Game />);
   },
   false
 );
