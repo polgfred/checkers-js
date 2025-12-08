ed62140705714ef283634c25a49a0441822c4e02
Frederick Polgardy <fred@polgardy.com>
2025-12-08 17:08:39 +0000

    simplify computer player logic

--- src/ui/computer_player.tsx
+++ src/ui/computer_player.tsx
@@ -2,15 +2,11 @@ import { useCallback, useContext, useEffect } from 'react';
 
 import { Board } from './board';
 import { GameContext } from './game_context';
-import { SideType } from '../core/types';
 
 export function ComputerPlayer() {
   const { board, side, makeMove } = useContext(GameContext);
 
-  const getMove = useCallback(async () => {
-    if (side === SideType.RED) {
-      return;
-    }
+  const handleMove = useCallback(async () => {
     const response = await fetch('/move', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -21,8 +17,8 @@ export function ComputerPlayer() {
   }, [board, side, makeMove]);
 
   useEffect(() => {
-    getMove();
-  }, [getMove]);
+    handleMove();
+  }, [handleMove]);
 
   return <Board board={board} />;
 }
