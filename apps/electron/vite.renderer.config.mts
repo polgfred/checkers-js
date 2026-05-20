242ce0558c4d34ac801f7923aa636892313f01e2
Frederick Polgardy <fred@polgardy.com>
2026-05-20 15:35:45 +0000

    don't inline svg images

--- apps/electron/vite.renderer.config.mts
+++ apps/electron/vite.renderer.config.mts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config
 export default defineConfig({
+  build: {
+    assetsInlineLimit: 0,
+  },
   plugins: [react()],
 });
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,5 +2,8 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    assetsInlineLimit: 0,
+  },
   plugins: [react()],
 });
