5656b8720d194f7358c019b0efd97da667b5da4c
Frederick Polgardy <fred@polgardy.com>
2026-07-01 13:11:58 -0600

    iterate over the board directly

--- packages/ui/src/board.tsx
+++ packages/ui/src/board.tsx
@@ -5,31 +5,30 @@ import { Piece } from './piece';
 import { Square } from './square';
 import styles from './styles.module.css';
 
-const COORDS = [0, 1, 2, 3, 4, 5, 6, 7];
-const REV_COORDS = COORDS.slice().reverse();
-
 export function Board({ board }: { board: BoardType }) {
   return (
     <div className={styles.boardStage}>
       <div className={`${styles.panelSurface} ${boardStyles.boardContainer}`}>
         <table className={boardStyles.boardTable}>
           <tbody>
-            {REV_COORDS.map((y) => (
-              <tr key={y}>
-                {COORDS.map((x) =>
-                  (x + y) % 2 === 0 ? (
-                    <Square key={x} x={x} y={y}>
-                      <span>{coordsToNumber(x, y)}</span>
-                      {board[y][x] === PieceType.EMPTY ? null : (
-                        <Piece x={x} y={y} p={board[y][x]} />
-                      )}
-                    </Square>
-                  ) : (
-                    <td key={x} />
-                  )
-                )}
-              </tr>
-            ))}
+            {board
+              .map((row, y) => (
+                <tr key={y}>
+                  {row.map((p, x) =>
+                    (x + y) % 2 === 0 ? (
+                      <Square key={x} x={x} y={y}>
+                        <span>{coordsToNumber(x, y)}</span>
+                        {p === PieceType.EMPTY ? null : (
+                          <Piece x={x} y={y} p={p} />
+                        )}
+                      </Square>
+                    ) : (
+                      <td key={x} />
+                    )
+                  )}
+                </tr>
+              ))
+              .reverse()}
           </tbody>
         </table>
       </div>
